792a19499f32eaf09c678e5574ef2291660d1fb8
KarateChap <gabrielmarianoofficial@gmail.com>
2023-05-04 18:14:06 +0800

    add date picker

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -3,12 +3,16 @@
 import { useRouter } from "next/navigation";
 import { Listing, Reservation } from "@prisma/client";
 
-import { SafeUser } from "@/app/types";
+import { SafeUser, SafeListing } from "@/app/types";
 import useCountries from "@/app/hooks/useCountries";
 import React, { useCallback, useMemo } from "react";
+import { format } from "date-fns";
+import Image from "next/image";
+import HeartButton from "../HeartButton";
+import Button from "../Button";
 
 interface ListingCardProps {
-  data: Listing;
+  data: SafeListing;
   reservation?: Reservation;
   onAction?: (id: string) => void;
   disabled?: boolean;
@@ -59,9 +63,66 @@ const ListingCard: React.FC<ListingCardProps> = ({
 
     const start = new Date(reservation.startDate);
     const end = new Date(reservation.endDate);
-  }, []);
 
-  return <div></div>;
+    return `${format(start, "PP")} - ${format(end, "PP")}`;
+  }, [reservation]);
+
+  return (
+    <div
+      onClick={() => router.push(`/listings/${data.id}`)}
+      className="
+        col-span-1
+        cursor-pointer
+        group
+    "
+    >
+      <div className="flex flex-col gap-2 w-full">
+        <div
+          className="
+                aspect-square
+                w-full
+                relative
+                overflow-hidden
+                rounded-xl
+            "
+        >
+          <Image
+            fill
+            alt="Listing"
+            src={data.imageSrc}
+            className="
+                object-cover
+                h-full
+                w-full
+                group-hover:scale-110
+                transition
+            "
+          />
+          <div className="absolute top-3 right-3">
+            <HeartButton listingId={data.id} currentUser={currentUser} />
+          </div>
+        </div>
+        <div className="font-semibold text-lg">
+          {location?.region}, {location?.label}
+        </div>
+        <div className="font-light text-neutral-500">
+          {reservationDate || data.category}
+        </div>
+        <div className="flex flex-row items-center gap-1">
+          <div className="font-semibold">${price}</div>
+          {!reservation && <div className="font-light">night</div>}
+        </div>
+        {onAction && actionLabel && (
+          <Button
+            disabled={disabled}
+            small
+            label={actionLabel}
+            onClick={handleCancel}
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default ListingCard;
